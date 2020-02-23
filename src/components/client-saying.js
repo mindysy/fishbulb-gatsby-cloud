0e0c6e3f32463fceb55d17155973bcd5654ec6d1
Mindy Alysa Sy <mindysy1990@gmail.com>
2020-02-24 07:25:09 +0800

    Add testimonials

--- src/components/client-saying.js
+++ src/components/client-saying.js
@@ -9,8 +9,8 @@ import "../../node_modules/aos/dist/aos.css" // You can also use <link> for styl
 
 const Saying = () => {
   useEffect(() => {
-    AOS.init();
-  }, []);
+    AOS.init()
+  }, [])
 
   const data = useStaticQuery(
     graphql`
@@ -38,9 +38,12 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="200"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “We couldn’t be happier with the custom studio management software
+          Fishbulb created for us! Staff efficiency has skyrocketed since its
+          implementation and customer engagement via our parent and student
+          portal has significantly reduced our administration hours.”
+          <span>Jodie Williams</span>
+          Director, Action Dance Academy
         </div>
 
         <div
@@ -48,9 +51,11 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="200"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “Having worked with Fishbulb Solutions for over 10 years we can
+          confidently assure you they’re a custom software development partner
+          you can trust.”
+          <span>Jock Germany</span>
+          Director, Environmental Treatment Solutions
         </div>
 
         <div
@@ -58,15 +63,11 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="300"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-          vero eros et accumsan et iusto odio.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “The custom project management application we had build saves us a
+          tonne of time managing our engineering projects. It’s well and truely
+          paid for itself and we’re extremely happy with the ROI.”
+          <span>Graeme Deaker</span>
+          Director, WSP Parsons Brinckerhoff
         </div>
 
         <div
@@ -74,21 +75,25 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="400"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “They are a fast, reliable, creative and innovative custom software
+          provider and the WHS mobile web app they build for us is extremely
+          intuitive.”
+          <span>Greg Aylett</span>
+          Manufacturing Excellence Manager, Vinidex
         </div>
 
         <div
-          className="clearfix aos-init tooltip tooltip--5 tooltip--right tooltip--small"
+          className="clearfix aos-init tooltip tooltip--5 tooltip--right"
           data-aos="fade-in"
           data-aos-offset="450"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “The team at Fishbulb extended the functionality of our SAP Business
+          One ERP software to include a custom barcode, labelling and despatch
+          application at a fraction of the cost quoted by other SAP business
+          partners. We're extremely pleased with service they provide and have
+          continued working with them on further projects.”
+          <span>Danny Gellert</span>
+          Director, Cello Paper Pty Ltd
         </div>
 
         <div
@@ -96,9 +101,13 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="500"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “Fishbulb Solutions helped rescue a FileMaker Pro project management
+          application built by an ex employee. Not only did they tidy up the
+          backend database and improve reporting capabilities, they also
+          improved the user interface making it vastly more intuitive and easy
+          to onboard new staff.”
+          <span>Ben North</span>
+          Managing Director, Waterman AHW Pty Ltd
         </div>
 
         <div
@@ -106,10 +115,13 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="500"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “We're extremely impressed with the custom data centre asset
+          management software Andrew and the team at Fishbulb Solutions created
+          for us. They very quickly deciphered our multitude of spreadsheets,
+          graphs and reports and guided us through the process of converting all
+          this into a stand-alone, intuitive, custom software system.”
+          <span>Michael Baker</span>
+          Vodafone Australia Pty Limited
         </div>
 
         <div
@@ -117,20 +129,26 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="500"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “Andrew and the team are really easy to work with. They're honest,
+          flexible and reliable. The custom software system and supplier portal
+          they've created is easy to use and has allowed us to grow quickly
+          whilst keeping our admin costs in check.”
+          <span>Simon Cook</span>
+          Director, Enso International
         </div>
 
         <div
-          className="clearfix aos-init tooltip tooltip--9 tooltip--up tooltip--small"
+          className="clearfix aos-init tooltip tooltip--9 tooltip--up tooltip--big"
           data-aos="fade-in"
           data-aos-offset="500"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit.”
-          <span>John Micheal Doe</span>
-          Director, Company Name
+          “We've worked with Fishbulb Solutions for over 3 years. We originally
+          contracted them to rescue our membership database project. They've
+          done a fantastic job and our member engagement and system satisfaction
+          has risen significantly. FB are now handling all our app, website and
+          database requirements and we wouldn't hesitate recommending them!”
+          <span>Kirstie Wells</span>
+          Global Office Manager, C3 Global
         </div>
 
         <div
@@ -141,7 +159,7 @@ const Saying = () => {
           “Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
           nonummy nibh euismod tincidunt ut laoreet dolore magna.”
           <span>John Micheal Doe</span>
-          Director, Company Name
+          Director, HQCorp
         </div>
         {/* End Client Testimony Tooltip */}
       </div>
