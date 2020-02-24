34ca386e8d16359c901548238135ab466eebbfe3
Mindy Alysa Sy <mindysy1990@gmail.com>
2020-02-24 09:09:02 +0800

    Adjust Testimonials

--- src/components/client-saying.js
+++ src/components/client-saying.js
@@ -34,34 +34,9 @@ const Saying = () => {
 
         {/* Client Testimony Tooltip */}
         <div
-          className="aos-init tooltip tooltip--1 tooltip--down tooltip--small"
+          className="aos-init tooltip tooltip--1 tooltip--down"
           data-aos="fade-in"
           data-aos-offset="200"
-        >
-          “We couldn’t be happier with the custom studio management software
-          Fishbulb created for us! Staff efficiency has skyrocketed since its
-          implementation and customer engagement via our parent and student
-          portal has significantly reduced our administration hours.”
-          <span>Jodie Williams</span>
-          Director, Action Dance Academy
-        </div>
-
-        <div
-          className="clearfix aos-init tooltip tooltip--2 tooltip--down tooltip--small"
-          data-aos="fade-in"
-          data-aos-offset="200"
-        >
-          “Having worked with Fishbulb Solutions for over 10 years we can
-          confidently assure you they’re a custom software development partner
-          you can trust.”
-          <span>Jock Germany</span>
-          Director, Environmental Treatment Solutions
-        </div>
-
-        <div
-          className="clearfix aos-init tooltip tooltip--3 tooltip--down tooltip--med"
-          data-aos="fade-in"
-          data-aos-offset="300"
         >
           “The custom project management application we had build saves us a
           tonne of time managing our engineering projects. It’s well and truely
@@ -71,21 +46,23 @@ const Saying = () => {
         </div>
 
         <div
-          className="clearfix aos-init tooltip tooltip--4 tooltip--left tooltip--big"
+          className="clearfix aos-init tooltip tooltip--2 tooltip--down tooltip--small"
           data-aos="fade-in"
-          data-aos-offset="400"
+          data-aos-offset="200"
         >
-          “They are a fast, reliable, creative and innovative custom software
-          provider and the WHS mobile web app they build for us is extremely
-          intuitive.”
-          <span>Greg Aylett</span>
-          Manufacturing Excellence Manager, Vinidex
+          “We've worked with Fishbulb Solutions for over 3 years. We originally
+          contracted them to rescue our membership database project. They've
+          done a fantastic job and our member engagement and system satisfaction
+          has risen significantly. FB are now handling all our app, website and
+          database requirements and we wouldn't hesitate recommending them!”
+          <span>Kirstie Wells</span>
+          Global Office Manager, C3 Global
         </div>
 
         <div
-          className="clearfix aos-init tooltip tooltip--5 tooltip--right"
+          className="clearfix aos-init tooltip tooltip--3 tooltip--down tooltip--med"
           data-aos="fade-in"
-          data-aos-offset="450"
+          data-aos-offset="300"
         >
           “The team at Fishbulb extended the functionality of our SAP Business
           One ERP software to include a custom barcode, labelling and despatch
@@ -97,7 +74,59 @@ const Saying = () => {
         </div>
 
         <div
+          className="clearfix aos-init tooltip tooltip--4 tooltip--left tooltip--big"
+          data-aos="fade-in"
+          data-aos-offset="400"
+        >
+          “We're extremely impressed with the custom data centre asset
+          management software Andrew and the team at Fishbulb Solutions created
+          for us. They very quickly deciphered our multitude of spreadsheets,
+          graphs and reports and guided us through the process of converting all
+          this into a stand-alone, intuitive, custom software system.”
+          <span>Michael Baker</span>
+          Vodafone Australia Pty Limited
+        </div>
+
+        <div
+          className="clearfix aos-init tooltip tooltip--5 tooltip--right"
+          data-aos="fade-in"
+          data-aos-offset="450"
+        >
+          “Andrew and the team are really easy to work with. They're honest,
+          flexible and reliable. The custom software system and supplier portal
+          they've created is easy to use and has allowed us to grow quickly
+          whilst keeping our admin costs in check.”
+          <span>Simon Cook</span>
+          Director, Enso International
+        </div>
+
+        <div
           className="clearfix aos-init tooltip tooltip--6 tooltip--left tooltip--small"
+          data-aos="fade-in"
+          data-aos-offset="500"
+        >
+          “They are a fast, reliable, creative and innovative custom software
+          provider and the WHS mobile web app they build for us is extremely
+          intuitive.”
+          <span>Greg Aylett</span>
+          Manufacturing Excellence Manager, Vinidex
+        </div>
+
+        <div
+          className="clearfix aos-init tooltip tooltip--7 tooltip--up tooltip--med"
+          data-aos="fade-in"
+          data-aos-offset="500"
+        >
+          “We couldn’t be happier with the custom studio management software
+          Fishbulb created for us! Staff efficiency has skyrocketed since its
+          implementation and customer engagement via our parent and student
+          portal has significantly reduced our administration hours.”
+          <span>Jodie Williams</span>
+          Director, Action Dance Academy
+        </div>
+
+        <div
+          className="clearfix aos-init tooltip tooltip--8 tooltip--up tooltip--big"
           data-aos="fade-in"
           data-aos-offset="500"
         >
@@ -111,44 +140,15 @@ const Saying = () => {
         </div>
 
         <div
-          className="clearfix aos-init tooltip tooltip--7 tooltip--up tooltip--med"
-          data-aos="fade-in"
-          data-aos-offset="500"
-        >
-          “We're extremely impressed with the custom data centre asset
-          management software Andrew and the team at Fishbulb Solutions created
-          for us. They very quickly deciphered our multitude of spreadsheets,
-          graphs and reports and guided us through the process of converting all
-          this into a stand-alone, intuitive, custom software system.”
-          <span>Michael Baker</span>
-          Vodafone Australia Pty Limited
-        </div>
-
-        <div
-          className="clearfix aos-init tooltip tooltip--8 tooltip--up tooltip--big"
-          data-aos="fade-in"
-          data-aos-offset="500"
-        >
-          “Andrew and the team are really easy to work with. They're honest,
-          flexible and reliable. The custom software system and supplier portal
-          they've created is easy to use and has allowed us to grow quickly
-          whilst keeping our admin costs in check.”
-          <span>Simon Cook</span>
-          Director, Enso International
-        </div>
-
-        <div
           className="clearfix aos-init tooltip tooltip--9 tooltip--up tooltip--big"
           data-aos="fade-in"
           data-aos-offset="500"
         >
-          “We've worked with Fishbulb Solutions for over 3 years. We originally
-          contracted them to rescue our membership database project. They've
-          done a fantastic job and our member engagement and system satisfaction
-          has risen significantly. FB are now handling all our app, website and
-          database requirements and we wouldn't hesitate recommending them!”
-          <span>Kirstie Wells</span>
-          Global Office Manager, C3 Global
+          “Having worked with Fishbulb Solutions for over 10 years we can
+          confidently assure you they’re a custom software development partner
+          you can trust.”
+          <span>Jock Germany</span>
+          Director, Environmental Treatment Solutions
         </div>
 
         <div
@@ -156,10 +156,11 @@ const Saying = () => {
           data-aos="fade-in"
           data-aos-offset="500"
         >
-          “Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna.”
-          <span>John Micheal Doe</span>
-          Director, HQCorp
+          “Having worked with Fishbulb Solutions for over 10 years we can
+          confidently assure you they’re a custom software development partner
+          you can trust.”
+          <span>Jock Germany</span>
+          Director, Environmental Treatment Solutions
         </div>
         {/* End Client Testimony Tooltip */}
       </div>
