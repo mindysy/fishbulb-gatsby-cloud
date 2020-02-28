745a9b136a4e552a1fb890e9af675666294e6486
Mindy Alysa Sy <mindysy1990@gmail.com>
2020-02-28 09:06:58 +0800

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
+import Helmet from "react-helmet"
 
 import Layout from "../components/layout"
-
 import Banner from "../components/banner"
 import Saying from "../components/client-saying"
 import WhatWeDo from "../components/what-we-do"
@@ -12,6 +12,17 @@ import PreFooter from "../components/pre-footer"
 const IndexPage = () => {
   return (
     <Layout>
+      <Helmet>
+        <title>Fishbulb Solutions – Custom Software Development</title>
+        {/* <meta
+          name="title"
+          content="Fishbulb Solutions – Custom Software Development"
+        />
+         <meta
+          name="description"
+          content="This website is so awesome that I really don't think you can handle how much awesome that is happening here."
+        /> */}
+      </Helmet>
       <Banner />
       <Saying />
       <WhatWeDo />
